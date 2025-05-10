4389c273f039d9d71f6df6526cf945dc62f25620
ldetmer <1771267+ldetmer@users.noreply.github.com>
2025-05-10 19:03:40 -0400

    check output dir

--- internal/container/container.go
+++ internal/container/container.go
@@ -71,18 +71,18 @@ func GenerateRaw(config *ContainerConfig, apiRoot, output, apiPath string) error
 }
 
 func GenerateLibrary(config *ContainerConfig, apiRoot, output, generatorInput, libraryID string) error {
-	args1 := []string{
-		"run",
-		"--rm",
-		"-v",
-		generatorInput + ":/generator-input",
-		"alpine",
-		"ls",
-		"-l",
-		"/generator-input"}
-
-	runCommand("ls", []string{"-la", generatorInput}...)
-	runCommand("docker", args1...)
+	/*args1 := []string{
+	"run",
+	"--rm",
+	"-v",
+	generatorInput + ":/generator-input",
+	"alpine",
+	"ls",
+	"-l",
+	"/generator-input"}*/
+	slog.Info("printoutput")
+	runCommand("ls", []string{"-la", output}...)
+	//runCommand("docker", args1...)
 
 	if apiRoot == "" {
 		return fmt.Errorf("apiRoot cannot be empty")
