be567550ec34566db353785a5ff41eed0b3b8d5d
Tomo Suzuki <suztomo@gmail.com>
2025-11-05 12:20:44 -0500

    feat(java): release stage to update pom.xml files (#2772)
    The 1st part of the release stage command for Java is updating pom.xml
    files.
    
    For https://github.com/googleapis/librarian/issues/2516

--- internal/container/java/release/release.go
+++ internal/container/java/release/release.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -18,14 +18,28 @@ package release
 import (
 	"context"
 	"log/slog"
+	"path/filepath"
 
 	"github.com/googleapis/librarian/internal/container/java/languagecontainer/release"
 	"github.com/googleapis/librarian/internal/container/java/message"
+	"github.com/googleapis/librarian/internal/container/java/pom"
 )
 
 // Stage executes the release stage command.
 func Stage(ctx context.Context, cfg *release.Config) (*message.ReleaseStageResponse, error) {
 	slog.Info("release-stage: invoked", "config", cfg)
-	// TODO(suztomo): implement release-stage.
-	return &message.ReleaseStageResponse{}, nil
+	response := &message.ReleaseStageResponse{}
+	for _, lib := range cfg.Request.Libraries {
+		for _, path := range lib.SourcePaths {
+			slog.Info("release-stage: processing library", "libraryID", lib.ID, "version", lib.Version, "sourcePath", path)
+			if err := pom.UpdateVersions(
+				cfg.Context.RepoDir,
+				filepath.Join(cfg.Context.RepoDir, path),
+				cfg.Context.OutputDir, lib.ID, lib.Version); err != nil {
+				response.Error = err.Error()
+				return response, err
+			}
+		}
+	}
+	return response, nil
 }
