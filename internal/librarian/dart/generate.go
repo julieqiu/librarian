bd0e4af4016f0a4aaff58f3da929b815aa0c5cc3
Julie Qiu <julie@golang.org>
2026-01-16 07:32:52 -0500

    fix

--- internal/librarian/dart/generate.go
+++ internal/librarian/dart/generate.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package dart provides functionality for generating and releasing Dart client
+// libraries.
 package dart
 
 import (
