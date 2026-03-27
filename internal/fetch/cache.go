a5183ba0aa447b27b8aeac8895b52aa7779f526e
Julie Qiu <julieqiu@google.com>
2026-03-26 22:17:01 -0400

    refactor(internal/fetch): unexport functions and fix names (#4870)
    Unexport functions that are only used inside internal/fetch.
    
    Remove "tarball" from non-tarball-specific function names and comments.

--- internal/fetch/cache.go
+++ internal/fetch/cache.go
@@ -85,7 +85,7 @@ func RepoDir(ctx context.Context, repo, commit, expectedSHA256 string) (string, 
 				if err := os.MkdirAll(outDir, 0755); err != nil {
 					return "", fmt.Errorf("failed creating %q: %w", outDir, err)
 				}
-				if err := ExtractTarball(tgz, outDir); err == nil {
+				if err := extractTarball(tgz, outDir); err == nil {
 					return outDir, nil
 				}
 			}
@@ -103,10 +103,10 @@ func RepoDir(ctx context.Context, repo, commit, expectedSHA256 string) (string, 
 	if err := os.MkdirAll(outDir, 0755); err != nil {
 		return "", fmt.Errorf("failed creating %q: %w", outDir, err)
 	}
-	if err := DownloadTarball(ctx, tgz, sourceURL, expectedSHA256); err != nil {
+	if err := download(ctx, tgz, sourceURL, expectedSHA256); err != nil {
 		return "", err
 	}
-	if err := ExtractTarball(tgz, outDir); err != nil {
+	if err := extractTarball(tgz, outDir); err != nil {
 		return "", fmt.Errorf("failed to extract tarball: %w", err)
 	}
 	return outDir, nil
