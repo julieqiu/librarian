fe19cada49f538c6e4336923da6c35414b988d5f
Julie Qiu <julie@golang.org>
2026-03-30 21:59:46 -0400

    go generate

--- cmd/librarian/doc.go
+++ cmd/librarian/doc.go
@@ -24,5 +24,198 @@ Usage:
 	librarian <command> [arguments]
 
 The commands are:
+
+# add
+
+NAME:
+
+	librarian add - add a new client library to librarian.yaml
+
+USAGE:
+
+	librarian add <apis...>
+
+OPTIONS:
+
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# generate
+
+NAME:
+
+	librarian generate - generate a client library
+
+USAGE:
+
+	librarian generate <library>
+
+OPTIONS:
+
+	--all       generate all libraries
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# bump
+
+NAME:
+
+	librarian bump - update versions and prepare release artifacts
+
+USAGE:
+
+	librarian bump <library>
+
+DESCRIPTION:
+
+	bump updates version numbers and prepares the files needed for a new release.
+
+	If a library name is given, only that library is updated. The --all flag updates every
+	library in the workspace. When a library is specified explicitly, the --version flag can
+	be used to override the new version.
+
+	Examples:
+	  librarian bump <library>           # update version for one library
+	  librarian bump --all               # update versions for all libraries
+
+OPTIONS:
+
+	--all             update all libraries in the workspace
+	--version string  specific version to update to; not valid with --all
+	--help, -h        show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# install
+
+NAME:
+
+	librarian install - install dependencies for the current language
+
+USAGE:
+
+	librarian install
+
+OPTIONS:
+
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# tidy
+
+NAME:
+
+	librarian tidy - format and validate librarian.yaml
+
+USAGE:
+
+	librarian tidy
+
+OPTIONS:
+
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# update
+
+NAME:
+
+	librarian update - update sources to the latest version
+
+USAGE:
+
+	librarian update <sources...>
+
+DESCRIPTION:
+
+	Supported sources are:
+	  - conformance
+	  - discovery
+	  - googleapis
+	  - protobuf
+	  - showcase
+
+OPTIONS:
+
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# version
+
+NAME:
+
+	librarian version - print the version
+
+USAGE:
+
+	librarian version
+
+OPTIONS:
+
+	--help, -h  show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# publish
+
+NAME:
+
+	librarian publish - publishes client libraries
+
+USAGE:
+
+	librarian publish
+
+OPTIONS:
+
+	--execute                fully publish (default is to only perform a dry run)
+	--release-commit string  the release commit to publish; default finds latest release commit
+	--dry-run                print commands without executing (legacy Rust-only flag)
+	--dry-run-keep-going     print commands without executing, don't stop on error (legacy Rust-only flag)
+	--skip-semver-checks     skip semantic versioning checks (legacy Rust-only flag)
+	--help, -h               show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
+
+# tag
+
+NAME:
+
+	librarian tag - tags a release commit based on the libraries published
+
+USAGE:
+
+	librarian tag
+
+OPTIONS:
+
+	--release-commit string  the release commit to tag; default finds latest release commit
+	--create-release-tag     whether to create a tag of the form release-{PR number}
+	--help, -h               show help
+
+GLOBAL OPTIONS:
+
+	--verbose, -v  enable verbose logging
 */
 package main
